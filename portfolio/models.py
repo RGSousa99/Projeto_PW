5974159118b16e9677af72dee0f59ed8ddb16b68
RGSousa99 <RGSousa99@gmail.com>
2022-06-08 09:05:12 +0100

    test img quizz 0.2

--- portfolio/models.py
+++ portfolio/models.py
@@ -41,12 +41,10 @@ class Cadeira(models.Model):
     def __str__(self):
         return self.nome
 
-
-
 class Projeto(models.Model):
     titulo = models.CharField(max_length=30)
     descricao = models.CharField(max_length=500)
-    imagem = models.ImageField()
+    imagem = models.ImageField(blank=True)
     realizacao = models.DateField()
     cadeira = models.ForeignKey(Cadeira, on_delete=models.CASCADE)
     participantes = models.ForeignKey(Colega, on_delete=models.CASCADE)
@@ -68,7 +66,7 @@ class PontuacaoQuizz(models.Model):
 class Noticia(models.Model):
     titulo = models.CharField(max_length=50)
     texto = models.CharField(max_length=500)
-    imagem = models.ImageField()
+    imagem = models.ImageField(blank=True)
     link = models.CharField(max_length=200)
 
     def __str__(self):
