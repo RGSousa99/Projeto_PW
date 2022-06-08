57c5bb3af524519ff79e159527e74ba7fb015f1f
RGSousa99 <RGSousa99@gmail.com>
2022-06-08 19:50:49 +0100

    instal requirements 0.2

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 STATIC_URL = '/portfolio/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('portfolio/static'))]  # novo
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # novo
+STATIC_ROOT = str(BASE_DIR.joinpath('venv'))  # novo
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # novo
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
