5a8679437cee950fce50e15eb075e41b494d42ef
RGSousa99 <RGSousa99@gmail.com>
2022-06-08 19:57:54 +0100

    reset

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'portfolio:login'
--- portfolio/forms.py
+++ portfolio/forms.py
@@ -10,7 +10,7 @@ class PostForm(ModelForm):
         fields = '__all__'
         # inserção de classes CSS para formatação de cada campo do formulário
         widgets = {
-            'autor': forms.TextInput(attrs={'placeholder': 'Kim Metnoku'}),
+            'autor': forms.TextInput(attrs={'placeholder': 'Miguel Silva'}),
             'titulo': forms.TextInput(attrs={'placeholder': 'Projeto de programação'}),
             'descricao': forms.Textarea(attrs={'size': '150', 'width': '100%', 'rows': '5', 'wrap': 'soft',
                                                'maxlenght': '150', 'style': 'width: 95%'}),
@@ -28,6 +28,60 @@ class PostForm(ModelForm):
     # texto auxiliar a um determinado campo do formulário
     help_texts = {
         'autor': 'indique o seu nome',
+        'titulo': 'indique qual o projeto em que trabalhou comigo',
+        'descricao': 'indique até 155 caracteres como foi a experiência'
+    }
+
+class ProjetoForm(ModelForm):
+    class Meta:
+        model = Projeto
+        fields = '__all__'
+        # inserção de classes CSS para formatação de cada campo do formulário
+        widgets = {
+
+            'titulo': forms.TextInput(attrs={'placeholder': 'Projeto de programação'}),
+            'descricao': forms.Textarea(attrs={'size': '150', 'width': '100%', 'rows': '5', 'wrap': 'soft',
+                                               'maxlenght': '150', 'style': 'width: 95%'}),
+
+        }
+
+        # texto a exibir junto à janela de inserção
+        labels = {
+
+            'titulo': 'Título',
+            'descricao': 'Descrição'
+        }
+
+    # texto auxiliar a um determinado campo do formulário
+    help_texts = {
+        'titulo': 'indique qual o projeto em que trabalhou comigo',
+        'descricao': 'indique até 155 caracteres como foi a experiência'
+    }
+
+class CadeiraForm(ModelForm):
+    class Meta:
+        model = Cadeira
+        fields = '__all__'
+        # inserção de classes CSS para formatação de cada campo do formulário
+        widgets = {
+
+            'titulo': forms.TextInput(attrs={'placeholder': 'Projeto de programação'}),
+            'descricao': forms.Textarea(attrs={'size': '150', 'width': '100%', 'rows': '5', 'wrap': 'soft',
+                                               'maxlenght': '150', 'style': 'width: 95%'}),
+
+        }
+
+        # texto a exibir junto à janela de inserção
+        labels = {
+
+            'titulo': 'Título',
+            'descricao': 'Descrição',
+
+        }
+
+    # texto auxiliar a um determinado campo do formulário
+    help_texts = {
+
         'titulo': 'indique qual o projeto em que trabalhou comigo',
         'descricao': 'indique até 155 caracteres como foi a experiência'
     }
